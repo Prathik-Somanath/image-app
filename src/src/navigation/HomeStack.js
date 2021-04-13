ed2d95be6db36aec3a08f9407e1f97881f0f34d4
Prathik-Somanath <prathik.s1999@gmail.com>
2021-04-13 23:08:09 +0530

    added bottom tab navigator

--- src/src/navigation/HomeStack.js
+++ src/src/navigation/HomeStack.js
@@ -1,13 +1,52 @@
 import React from 'react';
+import {View, Text} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
 import HomeScreen from '../screens/HomeScreen';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
-export default function HomeStack() {
+function HomeScreenStack() {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Home" component={HomeScreen} />
     </Stack.Navigator>
+  );
+}
+
+function ProfileScreenStack() {
+  return (
+    <View>
+      <Text>This is Profile</Text>
+    </View>
+  );
+}
+
+export default function HomeStack() {
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color, size}) => {
+          let iconName;
+          if (route.name === 'Home') {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name === 'Profile') {
+            // eslint-disable-next-line no-unused-vars
+            iconName = focused ? 'person-circle' : 'person-circle-outline';
+          }
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: 'tomato',
+        inactiveTintColor: 'gray',
+      }}>
+      <Tab.Screen name="Home" component={HomeScreenStack} />
+      <Tab.Screen name="Profile" component={ProfileScreenStack} />
+    </Tab.Navigator>
   );
 }
