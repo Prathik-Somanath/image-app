b2a0450c9c1093c8b3386622e3071c594898186e
Prathik-Somanath <prathik.s1999@gmail.com>
2021-04-13 15:52:36 +0530

    splash screen added, logo changed, app name changed

--- src/App.js
+++ src/App.js
@@ -2,111 +2,58 @@
  * Sample React Native App
  * https://github.com/facebook/react-native
  *
+ * Generated with the UI Kitten template
+ * https://github.com/akveo/react-native-ui-kitten
+ *
+ * Documentation: https://akveo.github.io/react-native-ui-kitten/docs
+ *
  * @format
- * @flow strict-local
  */
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+ import React, {useEffect} from 'react';
+ import {StyleSheet} from 'react-native';
+ import {
+   ApplicationProvider,
+   IconRegistry,
+   Layout,
+   Text,
+ } from '@ui-kitten/components';
+ import {EvaIconsPack} from '@ui-kitten/eva-icons';
+ import * as eva from '@eva-design/eva';
+ import SplashScreen from 'react-native-splash-screen'
+ 
+ /**
+  * Use any valid `name` property from eva icons (e.g `github`, or `heart-outline`)
+  * https://akveo.github.io/eva-icons
+  */
+ const App = () =>{
+   useEffect(() => {
+     SplashScreen.hide();
+   }, []);
+   return (
+     <>
+       <IconRegistry icons={EvaIconsPack} />
+       <ApplicationProvider {...eva} theme={eva.light}>
+         <Layout style={styles.container}>
+           <Text style={styles.text} category="h1">
+             Images
+           </Text>
+         </Layout>
+       </ApplicationProvider>
+     </>
+   );
+ };
+ 
+ const styles = StyleSheet.create({
+   container: {
+     flex: 1,
+     justifyContent: 'center',
+     alignItems: 'center',
+   },
+   text: {
+     textAlign: 'center',
+   },
+ });
+ 
+ export default App;
+ 
